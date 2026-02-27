2bcd937b29bdaa2ec6600f15a9a01138457cadd7
Sharonrific <Sharonrific@gmail.com>
2026-02-27 15:00:59 -0500

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,9 @@
+to_do = ["Buy yam today", "do the laundry", "study at noon", "Go to the gym"]
+user = input("Pick your task today")
+to_do.append(user)
+print("Task added successfully")
+
+for task in to_do:
+  print(task)
+
+

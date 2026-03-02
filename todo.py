b720fc6dc275624fc05693e035e7c2bad379c27c
sharonrific <sharondavid@moiconstruction.com>
2026-03-02 14:28:33 -0500

    To do list

--- todo.py
+++ todo.py
@@ -1,9 +1,49 @@
-to_do = ["Buy yam today", "do the laundry", "study at noon", "Go to the gym"]
-user = input("Pick your task today")
+to_do = ["call a client", "do the laundry", "study at noon", "Go to the gym"]
+
+for index, value in enumerate(to_do):
+   print(f"{index + 1}. {value}")
+
+user = input("Pick your task today: ")
 to_do.append(user)
-print("Task added successfully")
 
-for task in to_do:
-  print(task)
+print("Task added successfully!")
+
+for index, value in enumerate(to_do):
+   print(f"{index + 1}. {value}")
+
+choice = int(input("Enter the number you want to update: "))
+
+index = choice - 1
+if index >= 0 and index < len(to_do):
+   user2 = input("Enter a new task: ")
+to_do[index] = user2
+
+for index, value in enumerate(to_do):
+   print(f"{index + 1}. {value}")
+
+user3 = int(input("Enter the number you want to delete: "))
+index = choice - 1
+if index >= 0 and index < len(to_do):
+   to_do.pop(index)
+ 
+for index, value in enumerate(to_do):
+   print(f"{index + 1}. {value}")
+
+print("Task delete successully!")
+  
+   
 
 
+
+
+
+
+
+   
+
+
+      
+    
+      
+
+    
